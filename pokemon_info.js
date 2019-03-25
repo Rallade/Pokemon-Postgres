dfb8d7e58dcd03dc9bcb25914c15557e1560ced0
Rohan Bhayana <rallade@hotmail.com>
2019-03-25 21:36:36 +0000

    Improved queries to obtain more distinct data

--- pokemon_info.js
+++ pokemon_info.js
@@ -2,22 +2,21 @@ const db = require("./db_conn").db;
 
 getMovesfromName = async name => {
     moves = await db.any(
-        "SELECT moves.identifier FROM moves JOIN moveslearned ON moves.id = moveslearned.move_id JOIN pokemon ON pokemon.id = moveslearned.pokemon_id WHERE pokemon.identifier LIKE $1",
+        "SELECT DISTINCT moves.id, moves.identifier, type_id, power, pp, accuracy, priority, target_id, damage_class_id, effect_id, effect_chance FROM moves JOIN moveslearned ON moves.id = moveslearned.move_id JOIN pokemon ON pokemon.id = moveslearned.pokemon_id WHERE pokemon.identifier LIKE $1",
         name
     );
-    moves = moves.map(move => move.identifier);
-    moves = new Set(moves);
     return moves;
 };
 
 getMovesfromID = async id => {
     moves = await db.any(
-        "SELECT moves.identifier FROM moves JOIN moveslearned ON moves.id = moveslearned.move_id WHERE moveslearned.pokemon_id=$1",
+        "SELECT DISTINCT moves.id, moves.identifier, type_id, power, pp, accuracy, priority, target_id, damage_class_id, effect_id, effect_chance FROM moves JOIN moveslearned ON moves.id = moveslearned.move_id WHERE moveslearned.pokemon_id=$1",
         id
     );
-    moves = moves.map(move => move.identifier);
-    moves = new Set(moves);
     return moves;
 };
 
-getMovesfromID(475).then(data => console.log(data));
+getMovesfromID(387).then(data => console.log(data));
+
+exports.getMovesfromID = getMovesfromID;
+exports.getMovesfromName = getMovesfromName;
