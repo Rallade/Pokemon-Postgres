83c20e2830ea5fb91da8f230b2959c82f3886378
Rohan Bhayana <rallade@hotmail.com>
2019-03-26 01:25:13 +0000

    Abstracted SQL queries

--- pokemon_info.js
+++ pokemon_info.js
@@ -1,3 +1,4 @@
+const assert = require("assert");
 const db = require("./db_conn").db;
 
 getMovesfromName = async name => {
@@ -16,7 +17,51 @@ getMovesfromID = async id => {
     return moves;
 };
 
-getMovesfromID(387).then(data => console.log(data));
+getStatsfromID = async (id, level, EVs, Nature) => {
+    assert.equal(Nature.reduce((a, b) => a + b), 5);
+    assert.equal(EVs.reduce((a, b) => a + b), 508);
+    stats = await db.one("SELECT * FROM basestats WHERE pokemon_id=$1", id);
+    stats.hp = HP_base_to_stat(stats.hp, level, EVs[0]);
+    stats.attack = base_to_stat(stats.attack, level, EVs[1], Nature[0]);
+    stats.defense = base_to_stat(stats.defense, level, EVs[2], Nature[1]);
+    stats.specialattack = base_to_stat(
+        stats.specialattack,
+        level,
+        EVs[3],
+        Nature[2]
+    );
+    stats.specialdefense = base_to_stat(
+        stats.specialdefense,
+        level,
+        EVs[4],
+        Nature[3]
+    );
+    stats.speed = base_to_stat(stats.speed, level, EVs[5], Nature[4]);
+
+    return stats;
+};
+
+function HP_base_to_stat(base, level, EV) {
+    IV = 31;
+    stat = (base * 2 + IV + EV / 4) * (level / 100);
+    stat = Math.floor(stat);
+    stat += level + 10;
+    return stat;
+}
+
+function base_to_stat(base, level, EV, Nature) {
+    let IV = 31;
+    let stat = 2 * base + IV + EV / 4;
+    stat *= level / 100;
+    stat = Math.floor(stat + 5);
+    stat *= Nature;
+    stat = Math.floor(stat);
+    return stat;
+}
+
+getStatsfromID(475, 50, [4, 252, 0, 0, 0, 252], [1.1, 1, 0.9, 1, 1]).then(
+    data => console.log(data)
+);
 
 exports.getMovesfromID = getMovesfromID;
 exports.getMovesfromName = getMovesfromName;
